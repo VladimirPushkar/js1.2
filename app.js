8a53cbd69bf8fa6fae2b5d67c079e08ea9b8970a
Vladimir <vladimir@Air-admin.cust.communityfibre.co.uk>
2022-10-05 20:47:38 +0100

    1.0

---
+++ app.js
@@ -0,0 +1,22 @@
+/* ||||||||||||| */
+alert('Cложение')
+let aP = +prompt("First number");
+let bP = +prompt("Second number");
+alert( aP + bP );
+/* ||||||||||||| */
+alert('Вычитание')
+let aM = +prompt("First number");
+let bM = +prompt("Second number");
+alert( aM - bM );
+/* ||||||||||||| */
+alert('Умножение')
+let aY = +prompt("First number");
+let bY = +prompt("Second number");
+alert( aY * bY );
+/* ||||||||||||| */
+alert('Деление')
+let aD = +prompt("First number");
+let bD = +prompt("Second number");
+alert( aD / bD );
+/* ||||||||||||| */
+
